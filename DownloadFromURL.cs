3debf5d8f1df6bceaa0fc97fbbc540a46477887a
GenzyProject <Denrodin32@gmail.com>
2023-08-18 17:49:54 +0300

    Create DownloadFromURL.cs

---
+++ DownloadFromURL.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DownloadFromURL : MonoBehaviour
+{
+    [SerializeField] private private string url; //url to downloading file (google disk, etc...)
+    private string result;
+    
+    IEnumerator Start()
+    {
+        WWW wwwV = new WWW(urlVersion);
+        yield return wwwV;
+        txtVer = wwwV.text; 
+    }
+}
